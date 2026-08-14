c8fdded72e3ca1dbbca2b964842c927e959b2c99
elsayednegm13 <elsayedmohamed963@gmail.com>
2026-08-14 17:10:45 +0300

    DE

--- src/bootstrap/permission-catalog-v2.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// Later permission additions stay separate from the immutable v1 baseline.
-const INVENTORY_RETURN_RESTOCK_PERMISSIONS = [
-  "inventory.returns.approve_restock",
-];
-
-module.exports = { INVENTORY_RETURN_RESTOCK_PERMISSIONS };
