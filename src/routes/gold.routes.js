6c836e160baf1d72e66fa5decd6153c60fddd566
Elsayed Negm <85335551+elsayednegm13@users.noreply.github.com>
2026-08-27 23:12:11 +0300

    del

--- src/routes/gold.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const goldController = require("../controllers/gold.controller");
-
-const router = express.Router();
-
-router.get("/live", goldController.getLivePrice);
-
-module.exports = router;
--- src/services/customer-phone.service.js
+++
@@ -1,6 +0,0 @@
-function normalizePhone(phone) {
-  if (phone === null || phone === undefined) return "";
-  return String(phone).replace(/[^0-9]/g, "").replace(/^0+/, "");
-}
-
-module.exports = { normalizePhone };
